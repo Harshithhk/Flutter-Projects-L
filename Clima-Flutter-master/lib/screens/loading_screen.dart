03d6efdc3a97ea535860d2e5cbe11a218a1e4ab1
Harshithhk <60171634+Harshithhk@users.noreply.github.com>
2020-09-23 16:09:07 +0530

    messed up loading scren get method

--- Clima-Flutter-master/lib/screens/loading_screen.dart
+++ Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/weather.dart';
+import 'dart:convert';
+import 'location_screen.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,22 +13,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position =
-        await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  void getLocationWeather() {
+    double latitude;
+    double longitude;
+    var apiKey = 'bc24884b79beb3571eb94b5ea201096c';
+
+    void getLocation() async {
+      Location location = Location();
+      await location.getCurrentLocation();
+      latitude = location.latitude;
+      longitude = location.longitude;
+      getData();
+      print('https://api.openweathermap.org/data/2'
+          '.5/weather?lat=$latitude&lon=$longitude&appid'
+          '=$apiKey&unit=metric');
+    }
+  }
+
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
--- Clima-Flutter-master/lib/screens/location_screen.dart
+++ Clima-Flutter-master/lib/screens/location_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
+import 'dart:convert';
+import 'package:clima/services/weather.dart';
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weather = WeatherModel();
+  var temperature;
+  String weatherIcon;
+  String weatherMessage;
+  String cityName;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    setState(() {
+      temperature = jsonDecode(weatherData)['main']['temp'];
+      var condition = jsonDecode(weatherData)['weather'][0]['id'];
+      weatherIcon = weather.getWeatherIcon(condition);
+      weatherMessage = weather.getMessage(temperature);
+      cityName = jsonDecode(weatherData)['name'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,11 +76,11 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      ' $weatherIcon',
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -62,7 +89,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  '$weatherMessage in $cityName!',
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
